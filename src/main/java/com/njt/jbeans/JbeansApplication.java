64a98dbde6683ba429cae33c9374e251b2cbf9f1
cid-39 <carlindark39@gmail.com>
2026-05-20 18:43:50 +0200

    init

---
+++ src/main/java/com/njt/jbeans/JbeansApplication.java
@@ -0,0 +1,13 @@
+package com.njt.jbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njt/jbeans/JbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njt.jbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
